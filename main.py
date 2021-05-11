14aeafd4d03a7ecc8aafe200122f5ff6ab66cb29
MzoneCL <418867324@qq.com>
2021-05-11 17:04:32 +0800

    511下午

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ data_mode_path = 'data_processed/fill_with_mode.csv'
 
 def classification_report(method: str, Y_true, Y_pred):
     print(method)
+    print('Accuracy: ' + str(accuracy_score(Y_true,Y_pred)))
     print('Precision: ' + str(precision_score(Y_true, Y_pred)))
     print('Recall: ' + str(recall_score(Y_true, Y_pred)))
     print('F1: ' + str(f1_score(Y_true, Y_pred)))
@@ -21,10 +22,10 @@ def classification_report(method: str, Y_true, Y_pred):
 
 def get_data():
     data = pd.read_csv(data_mode_path)
-    data_X = data.drop(['学号/工号', '姓名', 'y_label', '量表得分05', '量表得分06', '量表得分07', '量表得分11', '量表得分15', '性别'], axis=1)
+    data_X = data.drop(['学号/工号', '姓名', 'y_label', '量表得分02'], axis=1)
     data_Y = data['y_label']
 
- #   data_X['性别'] = data_X['性别'].map({'男': 1, '女': 0})
+    data_X['性别'] = data_X['性别'].map({'男': 1, '女': 0})
 
    # print(data_X.info())
 
@@ -42,7 +43,7 @@ def use_xgb():
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-    xgb = XGBClassifier()
+    xgb = XGBClassifier(booster='gblinear')
     xgb.fit(X_train, Y_train)
 
     Y_pred = xgb.predict(X_test)
@@ -68,12 +69,13 @@ def use_svc():
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=5)
 
-    svc = SVC(kernel='sigmoid')
+    svc = SVC(kernel='poly')
     svc.fit(X_train, Y_train)
 
     Y_pred = svc.predict(X_test)
 
     classification_report('SVC', Y_test, Y_pred)
+
 
 if __name__ == '__main__':
     print('begin')
